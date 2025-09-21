0cc898b266e8661a8963834cfdd9b0d503b1fe4a
hammami1 <hseyf@hotmail.fr>
2025-09-21 02:16:20 +0100

    f2

---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('home/', views.home_view, name='home'),
+    path('about/', views.about_view, name='about'),
+    path("", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path('signup/', views.signup_view, name='signup'),
+
+]
