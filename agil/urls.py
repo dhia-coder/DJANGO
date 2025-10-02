f2ad5d00770bcf8f3cbd31056e286aeb36a5ad85
dhia-coder <dhiaeddine.trabelsi@esprit.tn>
2025-10-02 20:00:42 +0100

    h5

--- agil/urls.py
+++ agil/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('prediction/', include('agilapp.urls')),
     path('cv-check/', include('cvcheck.urls')),
     path('cluster/', include('clustering_app.urls')),
-    
+    path('', include('django_prometheus.urls')),
     path('metrics/', include('django_prometheus.urls')),  # Changed path to metrics/
     
  
--- main/urls.py
+++ main/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("logout/", views.logout_view, name="logout"),
     path('signup/', views.signup_view, name='signup'),
     path('metrics/', include('django_prometheus.urls')),
+    path('', include('django_prometheus.urls')),
 ]
